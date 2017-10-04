7481ce6a5cd62ef7ae361ab4bfe75d4708c7ccb1
Yunusemre Şentürk <yunusemre.senturk@pardus.org.tr>
2017-10-04 14:36:55 +0300

    Add cleanUp slot

--- src/singleinstance.cpp
+++ src/singleinstance.cpp
@@ -1,4 +1,25 @@
+/*****************************************************************************
+ *   Copyright (C) 2016 by Yunusemre Senturk                                 *
+ *   <yunusemre.senturk@pardus.org.tr>                                       *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .          *
+ *****************************************************************************/
+
 #include "src/singleinstance.h"
+#include <QApplication>
 
 SingleInstance::SingleInstance(QObject *parent) : QObject(parent)
 {
@@ -13,19 +34,18 @@ SingleInstance::~SingleInstance()
 bool SingleInstance::listen(QString name)
 {
     mServer.removeServer(name);
+    mServer.setSocketOptions(QLocalServer::WorldAccessOption);
     return mServer.listen(name);
 }
 
 bool SingleInstance::hasPrevious(QString name, QStringList arg)
-{
-
-    qDebug() << "Checking for previous instance...";
+{    
     QLocalSocket socket;
     socket.connectToServer(name, QLocalSocket::ReadWrite);
 
     if(socket.waitForConnected())
     {
-        qDebug() << "Sending args to previous instance...";
+        qInfo("Sending args to previous instance");
         QByteArray buffer;
 
         foreach(QString item, arg)
@@ -36,8 +56,6 @@ bool SingleInstance::hasPrevious(QString name, QStringList arg)
         socket.waitForBytesWritten();
         return true;
     }
-
-    qDebug() << socket.errorString();
 
     return false;
 
@@ -53,5 +71,11 @@ void SingleInstance::newConnection()
 void SingleInstance::readyRead()
 {   
     mSocket->deleteLater();
+}
+void SingleInstance::cleanUp()
+{
+    qDebug() << "Cleaning up";
+    mServer.close();
+    QApplication::quit();
 }
 
